a3dfaef58be7a1a059182b9739e6c9b1062b82d0
Toko Dougherty <Toko@Xx-2.local>
2016-03-23 21:57:32 -0700

    Transferred to the

--- README.md
+++
@@ -1,2 +0,0 @@
-# Kanye
-Web app url can be found here: https://kanye-heinrici.c9users.io
--- db/migrate/20160319052422_add_usernameto_users.rb
+++
@@ -1,6 +0,0 @@
-class AddUsernametoUsers < ActiveRecord::Migration
-  def change #anything here gets translated to SQL
-    add_column :users, :username, :string #add column usersnames to users (devise) table. 
-    add_index :users, :username, unique:true #index username for quick use, and make sure that it is always unique.
-  end
-end
